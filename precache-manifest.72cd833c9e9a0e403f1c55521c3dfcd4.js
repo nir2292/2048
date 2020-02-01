c31316aae5ae1d13b0e5123165717e1289ddaebe
Nir Erez <nir.erez@mail.huji.ac.il>
2020-02-01 18:01:03 +0200

    Updates

---
+++ precache-manifest.72cd833c9e9a0e403f1c55521c3dfcd4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2955a001ecddf544d815bba72696bce",
+    "url": "/2048game/index.html"
+  },
+  {
+    "revision": "240741d9f18b9067523e",
+    "url": "/2048game/static/css/main.0c1f67eb.chunk.css"
+  },
+  {
+    "revision": "c7f40fd27f2d7a23b9ca",
+    "url": "/2048game/static/js/2.22a7c691.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/2048game/static/js/2.22a7c691.chunk.js.LICENSE"
+  },
+  {
+    "revision": "240741d9f18b9067523e",
+    "url": "/2048game/static/js/main.3ffe0070.chunk.js"
+  },
+  {
+    "revision": "3b1625ce43c7c7f72480",
+    "url": "/2048game/static/js/runtime-main.af32b9ec.js"
+  }
+]);
